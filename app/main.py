f163a84716d497279f45c02d112eafa246a2ff52
koko <kevinarnaud974@hotmail.com>
2026-01-10 17:59:34 +0400

    added get_all_posts()

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
-# APP:
-app = FastAPI()
 
-
-# test root
-@app.get("/", status_code=status.HTTP_200_OK)
-async def root():
-    return {"message":"Welcome to my API!"}
 
     
 #===================================================================#
@@ -21,3 +17,41 @@ class POST(BaseModel):
    #created_at: ? 
 
    
+
+# Access to DB:
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="tutoDB",
+            user="postgres",
+            password="kottak",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("DATABSE CONNECTED!")
+        break
+    except Exception as e:
+        print(f"ERROR CONNECTION DB: {e}")
+        time.sleep(2)
+
+        
+# APP:
+app = FastAPI()
+
+
+# test root
+@app.get("/", status_code=status.HTTP_200_OK)
+async def root():
+    return {"message":"Welcome to my API!"}
+        
+#====================================================================#
+#================= CRUD ================================#
+
+# all posts:
+@app.get("/posts", status_code=status.HTTP_200_OK)
+def get_all_posts():
+    cursor.execute('SELECT * FROM posts;')
+    posts = cursor.fetchall()
+    print(posts)
+    return {"all_posts": posts}
