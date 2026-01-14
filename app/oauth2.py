2153f131827a1149628fd2edad2e1ba111687290
koko <kevinarnaud974@hotmail.com>
2026-01-14 23:25:15 +0400

    feature(auth): added all token logic, create, verify, get_current_user

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,11 @@
 from jose import jwt, JWTError
 from datetime import datetime, timezone, timedelta
+from app.schemas import TokenData, TokenOut
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
 
+# endpoint vers OU on doit se connecter ( se login)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 SECRET_KEY = "patate2000"
 ALGORITHM = "HS256"
@@ -17,3 +22,27 @@ def create_access_token(data: dict)->str:
         algorithm=ALGORITHM
     )
     return encoded_jwt
+
+
+def verify_access_token(token: str, cred_exception: HTTPException)->TokenData:
+    try:
+        payload = jwt.decode(token=token,
+                key=SECRET_KEY,
+                algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if not id:
+            raise cred_exception
+        
+        token_data = TokenData(id=id)
+        return token_data
+    except JWTError:
+        raise cred_exception 
+
+
+def get_current_user(token:str = Depends(oauth2_scheme)):
+    cred_except = HTTPException(
+       status_code=status.HTTP_401_UNAUTHORIZED,
+       detail=f"Could not validate credentials...",
+       headers={"WWW-Authenticate":"Bearer"}
+    )
+    return verify_access_token(token=token, cred_exception=cred_except)
